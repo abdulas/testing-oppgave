5c97d579fc710999c2d1815d4ffa5c78f2734635
abodixz <112077676+abdulas@users.noreply.github.com>
2023-10-07 01:54:03 +0200

    update

--- __tests__/Quiz.test.tsx
+++ __tests__/Quiz.test.tsx
@@ -12,12 +12,8 @@ test('Quiz viser spørsmål og svaralternativer', () => {
   expect(answerOption).toBeInTheDocument();
 });
 
-test('Quiz ends when all questions are answered', () => {
-  const { getByText, queryByText } = render(<Quiz />);
-  const answerButtons = document.querySelectorAll('li'); // Finn alle svaralternativer
-  answerButtons.forEach((button) => {
-    fireEvent.click(button);
-  });
-  const questionText = queryByText('Spørsmål 1:'); // Endre dette basert på hvordan spørsmålene vises
-  expect(questionText).not.toBeInTheDocument();
-});
+
+
+
+
+
