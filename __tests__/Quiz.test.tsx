3bf947ab1a9bf04ae3fd11f17ecb062b9d5e1ecd
abdulas <112077676+abdulas@users.noreply.github.com>
2023-10-12 02:09:19 +0200

    update

--- __tests__/Quiz.test.tsx
+++ __tests__/Quiz.test.tsx
@@ -2,6 +2,7 @@ import Quiz from '@/app/Quiz/Quiz';
 import { render, fireEvent, screen, act, renderHook } from '@testing-library/react';
 import data from '@/app/Quiz/questions.json';
 import { describe } from 'node:test';
+import { useAlternativState, usePoengState } from '@/app/Quiz/useQuizState';
 
 
 
@@ -84,7 +85,90 @@ describe('Checking if clicking on the options will lead you forward', () => {
 // ----------------------------------------------------------------------------
 
 
+describe("Hooks testing", () => {
 
+  it("starter med riktig innledende tilstand", () => {
+    const { result: poengResult } = renderHook(usePoengState);
+    const { result: alternativResult } = renderHook(useAlternativState);
+    render(<Quiz />);
+  
+    expect(alternativResult.current.alternativ).toBe(0);
+    expect(poengResult.current.poeng).toBe(0);
+  });
+
+})
+
+
+// Har bare denne testen som feiler, og jeg prøvde å endre på koden i UseQuizState.js, 
+// men testen blir aldri riktig, kan du gi meg tips eller forklaring på hvorfor testen feiler!
+// Jeg har sett at også koden virker som den skal og at ResetAlternativ funksjonen virker som den skal også!
+
+describe("tester useAlternativState ", ()=>{
+  it("se om resetAlternativ fungerer som den skal altså tilbakestiller alternativ", () => {
+    const { result } = renderHook(useAlternativState);
+  
+    act(() => {
+      result.current.setAlternativ(2);
+    });
+  
+    expect(result.current.alternativ).toBe(2);
+  
+    act(() => {
+      result.current.resetAlternativ(); 
+    });
+  
+    expect(result.current.alternativ).toBe(0); 
+  });
+  
+
+})
+
+
+describe("ser om økning av poeng virker som det skal", ()=>{
+  it("se om økPoeng virker som den skal altså legger til poeng", () => {
+    const {result} = renderHook(usePoengState);
+
+    act(()=>{
+      result.current.økPoeng();
+    })
+
+    expect(result.current.poeng).toBe(1);
+
+  })
+
+
+  it("må legge til poeng ved riktig svar", () => {
+    const { result: poengResult } = renderHook(usePoengState);
+    render(<Quiz />);
+  
+    act(() => {
+      // logikken for å svare på spørsmålene.
+      expect(document.querySelector('h1')).toHaveTextContent('Spørsmål 1');
+      fireEvent.click(screen.getByText('Hyperlink and Text Management Language'));
+      poengResult.current.økPoeng();
+      
+      // her kan man gjnta prosessen for flere spørsmål ved ønske, siden første spørsmål virker så vil de andre virke som de skal.
+    });
+  
+    // Sjekk poengsummen etter å ha besvart spørsmål.
+    expect(poengResult.current.poeng).toBe(1);
+  });
+
+
+  it("øker poengsummen ved riktig svar", () => {
+    const { result: poengResult } = renderHook(usePoengState);
+    render(<Quiz />);
+  
+    act(() => {
+      fireEvent.click(screen.getByText('Hyperlink and Text Management Language'));
+      poengResult.current.økPoeng();
+    });
+  
+    expect(poengResult.current.poeng).toBe(1);
+  });
+
+
+})
 
 
 
